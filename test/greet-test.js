9fca04a6ccbe54f1ff257427f8702e96a5f96723
jodies383 <jodies383@gmail.com>
2021-08-23 16:19:50 +0200

    Unit tests added

---
+++ test/greet-test.js
@@ -0,0 +1,62 @@
+const assert = require('assert');
+const greet = require('../greetFactory');
+
+describe('settings-bill', function () {
+
+    const greeting = greet();
+
+    describe('Greetings', function () {
+        it('should greet "HELLO, JODIE"', function () {
+            (greeting.greetMe('Jodie', 'English'),
+
+                assert.equal(greeting.returnMessage('HELLO, JODIE'), 'HELLO, JODIE'));
+        });
+        it('should greet "HALLÅ, AMY"', function () {
+            (greeting.greetMe('Amy', 'Swedish'),
+
+                assert.equal(greeting.returnMessage('HALLÅ, AMY'), 'HALLÅ, AMY'));
+        });
+        it('should greet "HALLO, PAUL"', function () {
+            (greeting.greetMe('Paul', 'Dutch'),
+
+                assert.equal(greeting.returnMessage('HALLO, PAUL'), 'HALLO, PAUL'));
+        });
+    });
+    describe('Storing Names', function () {
+
+        it('should return the list of names that have been greeted', function () {
+            greeting.addNames("Jodie", "English");
+            greeting.addNames("Paul", "Swedish");
+            greeting.addNames("Amy", "Dutch");
+            assert.deepEqual({ JODIE: 1, PAUL: 1, AMY: 1 }, greeting.getNames());
+        });
+    });
+    describe('Counter', function () {
+
+        it('should return the list of names that have been greeted', function () {
+            greeting.addNames("Jodie", "English");
+            greeting.addNames("Paul", "Swedish");
+            greeting.addNames("Amy", "Dutch");
+            assert.equal(3, greeting.theCount());
+
+        });
+        it('should test that the function is not counting duplicates', function () {
+            greeting.addNames("Jodie", "English");
+            greeting.addNames("Jodie", "Swedish");
+            greeting.addNames("Jodie", "English");
+            greeting.addNames("paul", "Swedish");
+            greeting.addNames("Paul", "Swedish");
+            greeting.addNames("Amy", "Dutch");
+            assert.equal(3, greeting.theCount());
+
+        });
+    });
+    describe('Return error message', function () {
+
+        it('should return the message "Please enter a valid name"', function () {
+            greeting.addNames("123", "English");
+
+            assert.equal("Please enter a valid name", greeting.removeValidName());
+        });
+    });
+});
